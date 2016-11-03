54034cbfd5697d1c828dfdc97df9fb064b81bc0b
Traci7822 <Tracir@gmail.com>
2016-11-03 17:13:44 -0600

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var recipe = {ingredient: 'amounts'}
+
+function addIngredient(recipe, ingredient, amounts){
+  recipe[ingredient] = amounts;
+  return recipe
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe
+}
+
+function updateIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe
+}
+
+function readRecipe(recipe){
+  for (var key in recipe){
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
